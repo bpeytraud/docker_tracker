60a40706aefa6307a8126232a380aed783cd68e5
bpeytraud <150447798+bpeytraud@users.noreply.github.com>
2024-03-01 15:18:03 +0100

    Update script.sh
    mise à jour script

--- script.sh
+++ script.sh
@@ -1,24 +1,16 @@
 #!/bin/bash
 
-dnf install -y \
-        mysql-server \
-        tuleap \
-        tuleap-theme-burningparrot \
-        tuleap-theme-flamingparrot \
-        tuleap-plugin-tracker
-        
-echo -e '[mysqld]\nsql-mode="NO_ENGINE_SUBSTITUTION"' > /etc/my.cnf.d/tuleap.cnf
-
-# Configuration de MySQL
-systemctl enable mysqld
-systemctl start mysqld
-
 # Demande à l'utilisateur d'entrer le mot de passe MySQL
 read -s -p "Entrez le mot de passe MySQL pour l'utilisateur root : " mysql_password
 echo
 
 # Demande à l'utilisateur d'entrer le nom du serveur Tuleap
 read -p "Entrez le nom du serveur Tuleap : " tuleap_server_name
+
+# Demande à l'utilisateur d'entrer le nom du serveur mysql serveur
+read -p "Entrez le nom du serveur mysql : " mysql_server
+
+#mysql_server="${mysql_server:-localhost}"
 
 # Configuration de MySQL avec le mot de passe fourni par l'utilisateur
 mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$mysql_password';"
@@ -27,5 +19,6 @@ mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$mysql_password';
 /usr/share/tuleap/tools/setup.sh \
   --configure \
   --server-name="$tuleap_server_name" \
-  --mysql-server=localhost \
+  --mysql-server=$mysql_server \
   --mysql-password="$mysql_password"
+  --assumeyes
