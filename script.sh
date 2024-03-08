796f2d48c409343b6aeacf172f51a03e5f76a95b
bpeytraud <150447798+bpeytraud@users.noreply.github.com>
2024-03-08 09:26:20 +0100

    Update script.sh
    supression de l'intéractivité avec l'utilisateur

--- script.sh
+++ script.sh
@@ -1,24 +1,14 @@
 #!/bin/bash
 
-# Demande à l'utilisateur d'entrer le mot de passe MySQL
-read -s -p "Entrez le mot de passe MySQL pour l'utilisateur root : " mysql_password
-echo
-
-# Demande à l'utilisateur d'entrer le nom du serveur Tuleap
-read -p "Entrez le nom du serveur Tuleap : " tuleap_server_name
-
-# Demande à l'utilisateur d'entrer le nom du serveur mysql serveur
-read -p "Entrez le nom du serveur mysql : " mysql_server
-
-#mysql_server="${mysql_server:-localhost}"
+mysql_server="${mysql_server:-localhost}"
 
 # Configuration de MySQL avec le mot de passe fourni par l'utilisateur
-mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$mysql_password';"
+mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';"
 
 # Configuration de Tuleap
 /usr/share/tuleap/tools/setup.sh \
   --configure \
-  --server-name="$tuleap_server_name" \
+  --server-name="$TULEAP_SERVER_NAME" \
   --mysql-server="$mysql_server" \
-  --mysql-password="$mysql_password" \
+  --mysql-password="$MYSQL_PASSWORD" \
   --assumeyes
